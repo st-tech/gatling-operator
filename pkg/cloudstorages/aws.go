64130b7647401719238357e7a38e4fef57b5b314
Yoichi Kawasaki <yokawasa@gmail.com>
2022-08-30 07:30:25 +0900

    Update pkg/cloudstorages/aws.go
    Co-authored-by: akitok <akito.kobayashi@zozo.com>

--- pkg/cloudstorages/aws.go
+++ pkg/cloudstorages/aws.go
@@ -8,7 +8,7 @@ type AWSCloudStorageProvider struct {
 	providerName string
 }
 
-func (p *AWSCloudStorageProvider) init(args []EnvVars) { /* do nothinig */ }
+func (p *AWSCloudStorageProvider) init(args []EnvVars) { /* do nothing */ }
 
 func (p *AWSCloudStorageProvider) GetName() string {
 	return p.providerName
